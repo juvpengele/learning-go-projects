ee2e435bdcadf52e15deada6640cd993f71292f0
Juvenal PENGELE <juvenal.pengele@gmail.com>
2025-05-20 18:07:15 -0400

    refactor: improve calculator logic and variable handling

--- Beginner/cli-calculator/main.go
+++ Beginner/cli-calculator/main.go
@@ -7,19 +7,18 @@ var operators = []string{"+", "-", "*", "/"}
 func main() {
 	fmt.Println("Welcome to the CLI Calculator!")
 
-	var calculatorStarted bool = false
-
 	var num1 float64 = 0
 	var operator string
-	var result float64 = 0
+	var result *float64
 	continueCmd := "y"
 
 	for continueCmd == "y" {
-		if calculatorStarted == false {
+		if result == nil {
+			num1 := new(float64)
 			fmt.Print("Enter first number: ")
-			fmt.Scanln(&result)
-		} else {
-			calculatorStarted = true
+			fmt.Scanln(num1)
+
+			result = num1
 		}
 
 		fmt.Print("Enter operator (+, -, *, /): ")
@@ -34,44 +33,58 @@ func main() {
 		fmt.Print("Enter second number: ")
 		fmt.Scanln(&num1)
 
-		result = calculate(num1, operator, result)
-		fmt.Printf("Result: %.2f\n", result)
+		calculatedResult := calculate(num1, operator, result)
+		result = &calculatedResult
+		
+		fmt.Println("=======================================")
+		fmt.Println("result: ", *result)
+		fmt.Println("========================================")
 
-		fmt.Print("Do you want to continue? (y/n or any other touch): ")
-		fmt.Scanln(&continueCmd)
+	
+		continueCmd = continueOperation()
+	} 
 
-		if continueCmd != "y" {
-			fmt.Print("Thank for using the calculator. Bye 😊")
-		}
-	}
-
+	fmt.Println("Exiting the calculator. Goodbye! 😊")
 }
 
-func calculate(num1 float64, operator string, result float64) float64 {
+func continueOperation() string {
+	var continueCmd string
+	fmt.Print("Do you want to continue? (y/n or any other touch): ")
+	fmt.Scanln(&continueCmd)
+
+	return continueCmd
+}
+
+func calculate(num1 float64, operator string, result *float64) float64 {
+	if result == nil {
+		result = new(float64)
+		*result = 0
+	}
+
 	switch operator {
 		case "+":
-			result += num1
+			*result += num1
 		case "-":
-			result -= num1
+			*result -= num1
 		case "*":
-			result *= num1
+			*result *= num1
 		case "/":
 			if num1 != 0 {
-				result /= num1
+				*result /= num1
 			} else {
 				fmt.Println("Error: Division by zero is not allowed.")
 			}
 		default:
 			fmt.Println("Error: Invalid operator.")
 		}
-	return result
+	return *result
 }
 
 func validateOperator(operator string) bool {
-		for _, op := range operators {
-			if operator == op {
-				return true
-			}
+	for _, op := range operators {
+		if operator == op {
+			return true
 		}
-		return false
+	}
+	return false
 }
