e0b99f0ffe151186b6ad1b013e8aa7b571f919be
Juvenal PENGELE <juvenal.pengele@gmail.com>
2025-07-12 18:55:29 -0400

    feat: implement calculator functionality with operator validation and result display

---
+++ Beginner/cli-calculator/calculator/calculator.go
@@ -0,0 +1,43 @@
+package calculator
+
+import (
+	"fmt"
+	"slices"
+)
+
+var operators = []string{"+", "-", "*", "/"}
+
+func ShowResult(key string,result *float64) {
+	fmt.Println("=======================================")
+	fmt.Println(key, *result)
+	fmt.Println("========================================")
+}
+
+func Calculate(num1 float64, operator string, result *float64) float64 {
+	if result == nil {
+		result = new(float64)
+		*result = 0
+	}
+
+	switch operator {
+		case "+":
+			*result += num1
+		case "-":
+			*result -= num1
+		case "*":
+			*result *= num1
+		case "/":
+			if num1 != 0 {
+				*result /= num1
+			} else {
+				fmt.Println("Error: Division by zero is not allowed.")
+			}
+		default:
+			fmt.Println("Error: Invalid operator.")
+		}
+	return *result
+}
+
+func ValidateOperator(operator string) bool {
+	return slices.Contains(operators, operator)
+}
--- Beginner/cli-calculator/main.go
+++ Beginner/cli-calculator/main.go
@@ -1,91 +1,59 @@
 package main
 
 import (
+	"cli-calculator/calculator"
 	"fmt"
-	"slices"
 )
 
-var operators = []string{"+", "-", "*", "/"}
+var YES_CMD = "y"
 
 func main() {
-	fmt.Println("Welcome to the CLI Calculator!")
+	fmt.Println("*********** Welcome to the CLI Calculator! ***********")
 
-	var num1 float64 = 0
+	var num2 float64 = 0
 	var operator string
 	var result *float64
-	continueCmd := "y"
+	continueCmd := YES_CMD
 
-	for continueCmd == "y" {
+	for continueCmd == YES_CMD {
 		if result == nil {
 			num1 := new(float64)
 			fmt.Print("Enter first number: ")
 			fmt.Scanln(num1)
 
 			result = num1
+		} else{
+			calculator.ShowResult("Current Result: ", result)
 		}
 
 		fmt.Print("Enter operator (+, -, *, /): ")
-		fmt.Scanln(&operator)
+		fmt.Scanln(&operator)	
 
-		isValidOperator := validateOperator(operator)
+		isValidOperator := calculator.ValidateOperator(operator)
 
 		if ! isValidOperator {
 			panic("You used an wrong operator")
 		}
 
 		fmt.Print("Enter second number: ")
-		fmt.Scanln(&num1)
+		fmt.Scanln(&num2)
 
-		calculatedResult := calculate(num1, operator, result)
+		calculatedResult := calculator.Calculate(num2, operator, result)
 		result = &calculatedResult
-		
-		showResult(result) 
 
-		continueCmd = continueOperation()
-	} 
+		calculator.ShowResult("Result: ", result)
+
+		continueCmd = ContinueOperation()
+	}
 
 	fmt.Println("Exiting the calculator. Goodbye! 😊")
 }
 
-func continueOperation() string {
+
+func ContinueOperation() string {
 	var continueCmd string
 	fmt.Print("Do you want to continue? (y/n or any other touch): ")
 	fmt.Scanln(&continueCmd)
 
 	return continueCmd
-}
-
-func showResult(result *float64) {
-	fmt.Println("=======================================")
-	fmt.Println("result: ", *result)
-	fmt.Println("========================================")
-}
-
-func calculate(num1 float64, operator string, result *float64) float64 {
-	if result == nil {
-		result = new(float64)
-		*result = 0
-	}
-
-	switch operator {
-		case "+":
-			*result += num1
-		case "-":
-			*result -= num1
-		case "*":
-			*result *= num1
-		case "/":
-			if num1 != 0 {
-				*result /= num1
-			} else {
-				fmt.Println("Error: Division by zero is not allowed.")
-			}
-		default:
-			fmt.Println("Error: Invalid operator.")
-		}
-	return *result
-}
-
-func validateOperator(operator string) bool {
-	return slices.Contains(operators, operator)
 }
